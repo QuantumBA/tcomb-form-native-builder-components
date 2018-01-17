81a371e62328af0a51e03ddc02f2f341bce7e37c
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2018-01-17 18:42:50 +0100

    Validation of JSON files with 100% lines coverage

--- __tests__/processSchema.js
+++ __tests__/processSchema.js
@@ -6,12 +6,50 @@ const processSchema = require('../lib/processSchema')
 afterEach(fetchMock.restore())
 
 
-test('remote `select` definition', async function()
+describe('remote `select` definition', function()
 {
-  fetchMock.getOnce('*', require('./fixtures/response.json'))
+  test('meta', async function()
+  {
+    fetchMock.getOnce('*', require('./fixtures/response.json'))
 
-  const result = await processSchema(require('./fixtures/schema.json'))
+    const result = await processSchema(require('./fixtures/schema.json'))
 
-  expect(fetchMock.lastUrl()).toBe('/blah?value=foo')
-  expect(result.enum).toEqual({'1234': 'loren ipsum'})
+    expect(fetchMock.lastUrl()).toBe('/blah?value=foo')
+    expect(result.enum).toEqual({'1234': 'loren ipsum'})
+  })
+
+  test('no meta', async function()
+  {
+    fetchMock.getOnce('*', require('./fixtures/response2.json'))
+
+    const result = await processSchema(require('./fixtures/schema2.json'))
+
+    expect(fetchMock.lastUrl()).toBe('/blah?value=foo')
+    expect(result.enum).toEqual({'1234': 'loren ipsum'})
+  })
+})
+
+describe('bad enum definition', function()
+{
+  test('complex object without mapping', async function()
+  {
+    const promise = processSchema(require('./fixtures/schema3.json'))
+
+    return expect(promise).rejects.toHaveProperty('message', '`enum` definition format is unknown')
+  })
+
+  test('string', async function()
+  {
+    const promise = processSchema(require('./fixtures/schema4.json'))
+
+    return expect(promise).rejects.toHaveProperty('message', '`json` must be array or object')
+  })
+})
+
+test('child nodes', async function()
+{
+  const result = await processSchema(require('./fixtures/schema5.json'))
+
+  expect(result.enum).toEqual({'id': 'label'})
+  expect(result.foo.enum).toEqual({'id': 'label'})
 })
